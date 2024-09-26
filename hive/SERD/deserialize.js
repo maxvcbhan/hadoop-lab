3f4b4065df2ee42f699e5860029ea9cde4d51500
max vorabhol <max003003003@gmail.com>
2024-09-26 20:35:18 +0700

    upload log

--- hive/SERD/deserialize.js
+++ hive/SERD/deserialize.js
@@ -9,6 +9,10 @@ fs.readFile('exampleObject.json', 'utf8', (err, data) => {
 
   // Parse the JSON string back to a JavaScript object
   const deserializedObject = JSON.parse(data);
+  console.log(deserializedObject["age"])
+  deserializedObject.score = deserializedObject.score.map((value)=> value+10)
+  
+  console.log(deserializedObject.score)
 
   // Log the object to verify deserialization
   console.log(deserializedObject);
